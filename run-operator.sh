a26893c7ebaf67447b3d159a1269430cbd6aba28
John Casey <jdcasey@commonjava.org>
2019-04-18 16:15:53 -0500

    fix playbook location in local operator script

--- run-operator.sh
+++ run-operator.sh
@@ -4,4 +4,4 @@ rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
-ansible-playbook -i ./hosts -e namespace=jdcasey-testing -e push_changes=yes ./playbooks/openshift-setup.yml
+ansible-playbook -i ./hosts -e namespace=jdcasey-testing -e push_changes=yes ./openshift-setup.yml
