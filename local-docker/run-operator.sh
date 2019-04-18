bd6cdac1909304db493fc9e9dbc5e1941de5a833
John Casey <jdcasey@commonjava.org>
2019-04-18 17:16:22 -0500

    fine tuning, fixing bugs

--- local-docker/run-operator.sh
+++ local-docker/run-operator.sh
@@ -4,4 +4,4 @@ rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
-ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey -e push_changes=true ./openshift-setup.yml
+ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true ./openshift-setup.yml
--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t local-operator local-docker
-docker run --rm -ti -v $HOME/.nos:/opt/config local-operator "$@"
+docker run --rm -ti -v $HOME/.nos:/opt/config:Z local-operator "$@"
