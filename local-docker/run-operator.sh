fbc9b8110fb58f08354dfccd0770f6c66ad393b0
John Casey <jdcasey@commonjava.org>
2019-04-18 18:02:32 -0500

    fixing a few bugs and trying to adjust ordering in teardown

--- local-docker/run-operator.sh
+++ local-docker/run-operator.sh
@@ -4,4 +4,5 @@ rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
+set -x
 ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true "$@"
