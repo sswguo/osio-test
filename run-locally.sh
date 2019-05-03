01e17e7ef59ecea8350e18226bfe02f716769ae9
John Casey <jdcasey@commonjava.org>
2019-05-03 16:56:20 -0500

    correct with_fileglob in pvc push, and make generated-files configurable (and mount in local docker run)

--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ ! -d /tmp/generated-files ]; then
+	mkdir -p /tmp/generated-files
+fi
+
 docker build -t local-operator local-docker
-docker run --rm -ti -v $HOME/.nos:/opt/config:Z local-operator "$@"
+docker run --rm -ti -v /tmp/generated-files:/tmp/generated-files:Z -v $HOME/.nos:/opt/config:Z local-operator "$@"
