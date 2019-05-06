18c0dff23f6815f283885a31a4542295b92d9e3f
John Casey <jdcasey@commonjava.org>
2019-05-06 11:26:47 -0500

    more flexibliity in local docker run, and fix webhook typo

--- local-docker/run-operator.sh
+++ local-docker/run-operator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+DO_RUN=${RUN:-true}
+echo "Run? ${RUN:true}"
+
 rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
-set -x
-ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true "$@"
+if [ $RUN == true ]; then
+	set -x
+	ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true "$@"
+else
+	exec /bin/bash -l
+fi
+
--- run-locally.sh
+++ run-locally.sh
@@ -5,4 +5,4 @@ if [ ! -d /tmp/generated-files ]; then
 fi
 
 docker build -t local-operator local-docker
-docker run --rm -ti -v /tmp/generated-files:/tmp/generated-files:Z -v $HOME/.nos:/opt/config:Z local-operator "$@"
+docker run --rm -ti -e RUN=$RUN -v /tmp/generated-files:/tmp/generated-files:Z -v $HOME/.nos:/opt/config:Z local-operator "$@"
