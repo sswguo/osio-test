a49283497b5f4467aeddf53c8df4e38604ec8dde
John Casey <jdcasey@commonjava.org>
2019-05-06 11:27:49 -0500

    fix conditional

--- local-docker/run-operator.sh
+++ local-docker/run-operator.sh
@@ -7,7 +7,7 @@ rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
-if [ $RUN == true ]; then
+if [ "$RUN" == "true" ]; then
 	set -x
 	ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true "$@"
 else
