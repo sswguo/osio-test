63af8aacd33f985c1444f96d059d451f3f7e7451
John Casey <jdcasey@commonjava.org>
2019-05-06 11:29:29 -0500

    fix conditional (again)

--- local-docker/run-operator.sh
+++ local-docker/run-operator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 DO_RUN=${RUN:-true}
-echo "Run? ${RUN:true}"
+echo "Run? $DO_RUN"
 
 rm -rf /opt/operations
 git clone https://github.com/jdcasey/osio-test.git /opt/operations
 
 cd /opt/operations
-if [ "$RUN" == "true" ]; then
+if [ "$DO_RUN" == "false" ]; then
+	echo "Starting debug shell"
+	exec /bin/bash -l
+else
+	echo "Running playbook"
+
 	set -x
 	ansible-playbook -i ./hosts -c local -e @/opt/config/osio-extras-jdcasey.yml -e push_changes=true "$@"
-else
-	exec /bin/bash -l
 fi
 
