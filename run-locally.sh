d5634225d5fe4e84c27bc2ff89eadc4874d6a418
John Casey <jdcasey@commonjava.org>
2019-05-06 15:51:58 -0500

    simplify setup/teardown using flag for openshift-simple-service, and make config extras configurable

--- run-locally.sh
+++ run-locally.sh
@@ -4,7 +4,9 @@ if [ ! -d /tmp/generated-files ]; then
 	mkdir -p /tmp/generated-files
 fi
 
-EXTRAS=osio-extras-$USER.yml
+EXTRAS=${EXTRAS:-osio-extras-$USER.yml}
+
+which docker || sudo yum install docker && sudo systemctl start docker
 
 docker build -t local-operator local-docker
 docker run --rm -ti -e RUN=$RUN -e EXTRAS_FILE=$EXTRAS -v /tmp/generated-files:/tmp/generated-files:Z -v $HOME/.nos:/opt/config:Z local-operator "$@"
